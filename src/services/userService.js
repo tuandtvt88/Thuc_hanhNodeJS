0f760b5c9bf0c111e08d03db8788c953bfef2440
tuandtvt88 <tuandtvt88@gmail.com>
2024-12-08 09:53:06 +0700

    lan11

---
+++ src/services/userService.js
@@ -0,0 +1,64 @@
+import db from "../models/index";
+import bcrypt, { hash } from 'bcryptjs';
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+            let isExist = await checkUserEmail(email);
+            if (isExist) {
+                //user already exists//
+
+                let user = await db.User.findOne({
+                    where: { email: email }
+                });
+                if (user) {
+                    //compare password
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'OK';
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Password is incorrect. Please try again!'
+                    }
+                } else {
+                    userData.errCode = 2;
+                    userData.errMessage = `User's not found. Please try again!`
+
+                }
+
+
+            } else {
+                //return error
+                userData.errCode = 1;
+                userData.errMessage = `Your email isn't exist in your system. Please try other email!`
+
+            }
+            resolve(userData)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail }
+            })
+            if (user) {
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+
+}
+module.exports = {
+    handleUserLogin: handleUserLogin
+}
