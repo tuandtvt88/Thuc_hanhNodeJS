83fb129f197b81df49cd2d4466cd500575b3677b
tuandtvt88 <tuandtvt88@gmail.com>
2024-12-08 16:58:11 +0700

    lan13

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ import bodyParser from 'body-parser';
 import viewEngine from './config/viewEngine';
 import initWebRoutes from './route/web';
 import connectDB from './config/connectDB';
+import cors from 'cors';
 require('dotenv').config();
 
 let app = express();
+app.use(cors({ credentials: true, origin: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +17,6 @@ initWebRoutes(app);
 connectDB();
 
 let port = process.env.PORT || 6969;
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log("Server is running on port :" + port);
 })
